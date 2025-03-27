9a59a3457b086b676e2fbc94402997ca19481929
Splix1 <austinTgautney@gmail.com>
2025-03-27 14:52:15 -0700

    refactor: combined start and end date

--- services/NASA/NEOWebService.ts
+++ services/NASA/NEOWebService.ts
@@ -3,16 +3,16 @@ import { environment } from "../../environment";
 const apiKey = environment.nasaApiKey;
 const apiUrl = 'https://api.nasa.gov/neo/rest/v1/feed';
 
-export const getNEOsByDate = async (startDate: string, endDate: string) => {
+export const getNEOsByDate = async (date: string) => {
     try {
-        const response = await fetch(`${apiUrl}?start_date=${startDate}&end_date=${endDate}&api_key=${apiKey}`);
+        const response = await fetch(`${apiUrl}?start_date=${date}&end_date=${date}&api_key=${apiKey}`);
         
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
         
         const data = await response.json();
-        const NEOs = data.near_earth_objects[`${startDate}`];
+        const NEOs = data.near_earth_objects[`${date}`];
         
         return NEOs;
     } catch (error) {
